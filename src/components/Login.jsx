4af38cfebc446228aed4b1cb242edcc471231a50
Long3e5 <nguyenhienlong26@gmail.com>
2023-04-07 19:57:01 +0700

    first update

---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+export default function Login() {
+    return (
+        <Container>
+            <img 
+            src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png" 
+            alt="spotify" 
+            />
+            <button>Connect Spotify</button>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-dirextion: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vh;
+    background-color: #ldb954;
+    gap: 5rem;
+    img {
+        height:20vh;
+    }
+    button {
+        padding: 1rem 5rem;
+        border-radius: 5rem;
+    }
+`;
