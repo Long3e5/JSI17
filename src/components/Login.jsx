78cb252714d5a50ca1aab7b3bac2b9b17315e0f8
long3e5 <nguyenhienlong@gmail.com>
2023-04-14 19:44:29 +0700

    udate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+
 export default function Login() {
+    const HandleClick = async () => {
+        const client_id = "7af1ee6808ac43c98661e3a716dca85d";
+        const redirect_uri = "http://localhost:3000/";
+        const api_uri = "http://accounts.spotify.com/authorize/";
+        const scope = [
+            "user-read-email",
+            "user-read-private",
+            "user-read-playback-state",
+            "user-modify-playback-state",
+            "user-read-currently-playing",
+            "user-read-playback-position",
+            "user-top-read",
+            "user-read-recently-played",
+        ];
+        window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+            " "
+        )}&response_type=token&show_dialog=true`;
+    };
     return (
         <Container>
-            <img 
-            src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png" 
-            alt="spotify" 
-            />
-            <button>Connect Spotify</button>
+            <img src={require('./nigger.png')} alt="suckmycotify"/>
+            <button onClick={HandleClick}>Connect Suckmycotify</button>
         </Container>
     );
 }
 
 const Container = styled.div`
     display: flex;
-    flex-dirextion: column;
-    align-items: center;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
     height: 100vh;
-    width: 100vh;
-    background-color: #ldb954;
+    width: 100vw;
+    background-color: #1db954;
     gap: 5rem;
     img {
         height:20vh;
@@ -27,5 +43,10 @@ const Container = styled.div`
     button {
         padding: 1rem 5rem;
         border-radius: 5rem;
+        border: none;
+        background-color: black;
+        color: #49f585;
+        font-size: 1.4rem;
+        cursor: pointer;
     }
 `;
